87662558733d327e3412db69376c4c66e4d00ba4
Rajat Sundriyal <rajatsundriyal1@gmail.com>
2023-11-22 16:01:18 +0530

    fixes

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import NavBar from "../components/Navbar";
+import NavBar from "../components/NavBar";
 import SideBar from "../components/SideBar";
 import SearchCard from "../components/SearchCard";
 import { useAppDispatch, useAppSelector } from "../hooks/useApp";
